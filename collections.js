3620ae4fecf5705e10918ec08239441e82d4edd7
Jessica Lawshe <jessica@jlawshe.com>
2015-09-03 17:44:08 -0500

    removed console log

--- collections.js
+++ collections.js
@@ -4,15 +4,12 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 	Meteor.startup(function () {
 		Meteor.publish('userData', function() {
-			console.log('..userData');
 			if(!this.userId){
 				return this.ready();
 			}else{
 				var info = Meteor.users.find(this.userId, {fields: {
 					chill: 1
 				}});
-				console.log('info');
-				console.log(info);
 				return info;
 			}
 		});		
