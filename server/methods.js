9393d63bb8abe6da97a947eb1446cc0bf8c4bf75
Jessica Lawshe <jessica@jlawshe.com>
2015-09-03 17:28:22 -0500

    api path change

--- server/methods.js
+++ server/methods.js
@@ -1,7 +1,8 @@
 Meteor.methods({
 	'getIssueJson': function(v,i){
 		this.unblock();
-		var request = 'http://www.impactjournals.com/rss/?journal=oncotarget&v='+v+'&i='+i;
-		return Meteor.http.get(request);
+		var request = 'http://www.impactjournals.com/rss/index.php?journal=oncotarget&v='+v+'&i='+i;
+		var res = Meteor.http.get(request);
+		return res;
 	}
 });
