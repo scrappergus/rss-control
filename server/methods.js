4ed401f5b661198dbe86619b43b0a403c84a0219
Jessica Lawshe <jessica@jlawshe.com>
2015-09-03 17:41:10 -0500

    RSS - advanced query by issue id
    multiple issues with vol 5, num 0

--- server/methods.js
+++ server/methods.js
@@ -1,7 +1,11 @@
 Meteor.methods({
 	'getIssueJson': function(v,i){
 		this.unblock();
-		var request = 'http://www.impactjournals.com/rss/index.php?journal=oncotarget&v='+v+'&i='+i;
+		var param = '&v='+v+'&i='+i;
+		if(v === 5 && i === 0){
+			param = '&issue_id=79';
+		}
+		var request = 'http://www.impactjournals.com/rss/index.php?journal=oncotarget'+param;
 		var res = Meteor.http.get(request);
 		return res;
 	}
