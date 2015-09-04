ffa98aa34e923c49228c3e5ef6366fd43052a072
Jessica Lawshe <jessica@jlawshe.com>
2015-09-04 17:14:33 -0500

    new collection - feeds
    only users with ‘chill’ access can update/insert/remove from collection
    
    curentFeed - todo: publish, limit to 1

--- collections.js
+++ collections.js
@@ -1,7 +1,12 @@
+feeds = new Mongo.Collection('feeds');
+
 if (Meteor.isClient) {
 	Meteor.subscribe('userData');
+	Meteor.subscribe('feeds');
+	Meteor.subscribe('currentFeed');
 }
 if (Meteor.isServer) {
+	//PUBLISH
 	Meteor.startup(function () {
 		Meteor.publish('userData', function() {
 			if(!this.userId){
@@ -12,6 +17,41 @@ if (Meteor.isServer) {
 				}});
 				return info;
 			}
+		});	
+		Meteor.publish('feeds', function() {
+			if(!this.userId){
+				return this.ready();
+			}else{
+				var feedsList = feeds.find();
+				return feedsList;
+			}
+		});			
+		Meteor.publish('currentFeed', function() {
+			var currentFeed = feeds.find();
+			return currentFeed;
 		});		
 	});
+
+	//RULES
+	feeds.allow({
+		insert: function (userId, doc) {
+			console.log('insert!');
+			if(userId){
+				var u = Meteor.users.findOne({_id:userId});
+				return u.chill ;
+			}
+		},
+		update: function (userId, doc, fields, modifier) {
+			if(userId){
+				var u = Meteor.users.findOne({_id:userId});
+				return u.chill ;
+			}
+		},
+		remove: function (userId, doc) {
+			if(userId){
+				var u = Meteor.users.findOne({_id:userId});
+				return u.chill ;
+			}
+		}
+	});	
 }
