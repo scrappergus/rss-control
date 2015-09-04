b9e3abf847cf8c59ffce37cd5eee280e508691b5
Jessica Lawshe <jessica@jlawshe.com>
2015-09-04 17:16:56 -0500

    events to submit RSS
    translates JSON to XML
    sets pubDate of feed to current time
    redirect user to past feed page, if saved

--- client/js/events.js
+++ client/js/events.js
@@ -2,6 +2,7 @@ if (Meteor.isClient) {
 	Template.new.events({
 		'click #get-submit': function(e,t){
 			e.preventDefault();
+			Meteor.api.submit(e);
 			var i = t.find('#issue').value,
 				v = t.find('#volume').value;
 			if(!i){
@@ -16,24 +17,57 @@ if (Meteor.isClient) {
 					if(error){
 						Meteor.api.errors(error.message);
 					}else{
-						Meteor.api.success(results.content,'Volume '+v+', Issue '+i);
+						Session.set('rssDescription','Volume ' + v + ', Issue ' + i)
+						Meteor.api.success(results.content);
 					}
 			    });
 			}
 		},
 		'click #get-advance': function(e){
 			e.preventDefault();
+			Meteor.api.submit(e);
 			Meteor.call('getIssueJson',5,0, function(error, results) {
 				if(error){
 					Meteor.api.errors(error.message);
 				}else{
-					Meteor.api.success(results.content,'Advance Online');
+					Session.set('rssDescription','Advance Online')
+					Meteor.api.success(results.content);
 				}
 			});
 		},
 		'click #get-again': function(e){
 			e.preventDefault();
 			Meteor.api.retry();
+		},
+		'click #send-rss': function(e){
+			e.preventDefault();
+			var rssDescription = Session.get('rssDescription');
+			var json = Session.get('json');
+			json = JSON.parse(json);
+
+			//articles
+			var articles = [];
+			var feedLength = json.length;
+			for(var i = 0 ; i < feedLength ; i++){
+				var pii = parseInt(json[i]['pii']);
+				articles.push(pii);
+			}
+
+			//xml and pubdate
+			var result = Meteor.rss.objectToXML(json);
+			var xml = result['xml'];
+			var pubDate = result['pubDate'];
+			var date = result['pubDateDate'];
+
+			feeds.insert({'xml' : xml,'articles' : articles, 'pubDate' : pubDate, 'date' : date, 'description' : rssDescription},function(error,result){
+				if(error){
+					console.log(error);
+					alert(error.message);
+				}else{
+					Router.go('pastSingle',{'_id':result});
+				}
+			});
+			
 		}
 	});
 };
--- client/js/fxns.js
+++ client/js/fxns.js
@@ -1,11 +1,15 @@
 Meteor.api = {
+	submit: function(e){
+		// $('.form-btn').disabled = true;
+	},
 	errors: function(msg){
 		$('#msg-error').removeClass('hide');
 		$('#msg-error').html(msg);
 	},
-	success: function(json,header){
+	success: function(json){
+		var rssDescription = Session.get('rssDescription');
 		$('#msg-error').addClass('hide');
-		$('#v-i').html(header);
+		$('#v-i').html(rssDescription);
 		$('#get-form').addClass('hide');
 		// $('#data').html(json);
 		$('#res').removeClass('hide');
@@ -50,4 +54,32 @@ Meteor.api = {
 		// console.log(articles);
 		return articles;
 	}
+}
+
+Meteor.rss = {
+	objectToXML: function(json) {
+		var rssDescription = Session.get('rssDescription');
+		var result = {};
+		var today = new Date();
+		var UTCstring = today.toUTCString();
+		console.log(UTCstring);
+    	result['pubDate'] = UTCstring;
+    	result['pubDateDate'] = today;
+    	result['xml'] = '<?xml version="1.0"?><rss version="2.0"><channel>';
+    	result['xml'] += '<pubDate>' + UTCstring + '</pubDate>'; 
+    	result['xml'] += '<title>Oncotarget</title><language>en-us</language><link>http://www.impactjournals.com/oncotarget/index.php?journal=oncotarget</link><description>' + rssDescription + '</description>';
+
+    	var articlesCount = json.length;
+    	for(var i=0 ; i < articlesCount ; i++){
+    		var article = '<item>';
+    		article += '<title>'+json[i]['title']+'</title>';
+    		article += '<link>http://www.impactjournals.com/oncotarget/misc/linkedout.php?pii='+json[i]['pii']+'</link>';
+    		article += '<description>'+json[i]['authorsString']+'</description>';  
+    		article += '</item>';
+			result['xml'] += article;	
+    	}
+    	result['xml'] += '</channel></rss>';
+		return result;
+	},
+
 }
